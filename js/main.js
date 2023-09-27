73f205fa5abf78d2e9142d0bee5ed2c5c866acfe
Dani Ocaranza <docaranzan@gmail.com>
2023-09-26 21:53:50 -0300

    added resize canvas and clear canvas functions

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
 const CANVAS = document.querySelector('.canvas');
+let buttons = document.querySelectorAll('.btn');
+let menu = [resizeCanvas, clearCanvas];
 let pixels;
 let gridSize = 16;
 init(gridSize);
 
+buttons.forEach((button, index) => {
+  button.addEventListener('click', menu[index])
+});
 
+function resizeCanvas() {
+  let newGridSize;
+  while (true) {
+    newGridSize = prompt('What size grid do you want?');
+    if (newGridSize === null){
+      return;
+    }
+    if (newGridSize > 0 && newGridSize  < 101) {
+      break;
+    }
+    if (typeof newGridSize !== 'number') {
+      alert("Invalid input... Please insert numbers");
+      continue;
+    }
+
+    alert(`Invalid input... The size of the grid has to 
+          be between 1 and 100...`);
+  }
+
+  gridSize = Math.floor(newGridSize);
+
+  init(gridSize);
+}
+
+function clearCanvas() {
+  init(gridSize);
+}
 
 function createListeners(){
   let pixels = document.querySelectorAll('.pixel');
@@ -22,12 +54,12 @@ function init(size) {
   pixels = createListeners();
 }
 
-function generateGrid(gridSize) {
+function generateGrid(size) {
   CANVAS.replaceChildren();
-  for (let i = 0; i < gridSize; i++) {
+  for (let i = 0; i < size; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
-    for (let j = 0; j < gridSize; j++) {
+    for (let j = 0; j < size; j++) {
       let pixel = document.createElement('div');
       pixel.classList.add('pixel');
       row.appendChild(pixel);
